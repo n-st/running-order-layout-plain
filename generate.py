d2ba8bd93cc7c4b907d03c58023195a5dc29aa6e
Nils Steinger <git@n-st.de>
2022-07-14 18:09:47 +0200

    CSV reading and rendering

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@
 
 import sys
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+import csv
 
 def main():
     env = Environment(
@@ -10,27 +11,45 @@ def main():
         autoescape=select_autoescape()
     )
     template = env.get_template("schedule.html.j2")
-    content = {
-            'days': [
-                {
-                    'name': 'Wed, 28.08.',
-                    'stages': [
-                        {
-                            'name': 'Main',
-                            'bands': [
 
-                                ]
-                        },
-                        {
-                            'name': 'Wera',
-                            'bands': [
+    content = {'days': {}}
 
-                                ]
-                            }
-                    ]
-                    }
-                ]
+    with open('shows.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        for row in reader:
+            # Example row:
+            # 17.08.;Main;01:00;02:00;DIE APOKALYPTISCHEN REITER;genre;DE;1984
+            fields = dict(enumerate(row))
+            day = fields.get(0, 'unknown day')
+            stage = fields.get(1, 'unknown stage')
+            start = fields.get(2, '12:34')
+            starth, startm = [int(x) for x in start.split(':')]
+            end = fields.get(3, '12:35')
+            endh, endm = [int(x) for x in end.split(':')]
+            name = fields.get(4, '[no band name]')
+            genre = fields.get(5, '')
+            country = fields.get(6, '')
+            year = fields.get(7, '')
+            if day not in content['days']:
+                content['days'][day] = {
+                    'stages': {}
+                }
+            if stage not in content['days'][day]['stages']:
+                content['days'][day]['stages'][stage] = {
+                    'bands': []
+                }
+            band = {
+                'name': name,
+                'starth': starth,
+                'startm': startm,
+                'endh': endh,
+                'endm': endm,
+                'genre': genre,
+                'country': country,
+                'year': year,
             }
+            content['days'][day]['stages'][stage]['bands'].append(band)
+
     print(template.render(content))
 
 
