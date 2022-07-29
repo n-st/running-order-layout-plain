6def5beb3c1a9ebdfd6fdb98d9db6107241ac761
Nils Steinger <git@n-st.de>
2022-07-29 02:14:07 +0200

    Read CSV data from stdin

--- generate.py
+++ generate.py
@@ -22,51 +22,51 @@ def main():
 
     content = {'days': {}}
 
-    with open('shows.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=';')
-        for row in reader:
-            # Example row:
-            # 17.08.;Main;01:00;02:00;DIE APOKALYPTISCHEN REITER;genre;DE;1984
-            fields = dict(enumerate(row))
-            day = fields.get(0, 'unknown day')
-            stage = fields.get(1, 'unknown stage')
-            start = fields.get(2, '12:34')
-            starth, startm = [int(x) for x in start.split(':')]
-            end = fields.get(3, '12:35')
-            endh, endm = [int(x) for x in end.split(':')]
-            if starth < DAY_ROLLOVER_HOUR:
-                starth += 24
-            if endh < DAY_ROLLOVER_HOUR:
-                endh += 24
-            duration = (endh - starth) * 60 + (endm - startm)
-            name = fields.get(4, '[no band name]')
-            genre = fields.get(5, '')
-            country = fields.get(6, '')
-            year = fields.get(7, '')
-            if day not in content['days']:
-                content['days'][day] = {
-                    'stages': {},
-                    'minhour': 24,
-                    'maxhour': 0,
-                }
-            content['days'][day]['minhour'] = min(content['days'][day]['minhour'], starth)
-            content['days'][day]['maxhour'] = max(content['days'][day]['maxhour'], endh)
-            if stage not in content['days'][day]['stages']:
-                content['days'][day]['stages'][stage] = {
-                    'bands': []
-                }
-            band = {
-                'name': name,
-                'starth': starth,
-                'startm': startm,
-                'endh': endh,
-                'endm': endm,
-                'duration': duration,
-                'genre': genre,
-                'country': country,
-                'year': year,
+    csv_data = sys.stdin.read().splitlines()
+    reader = csv.reader(csv_data, delimiter=';')
+    for row in reader:
+        # Example row:
+        # 17.08.;Main;01:00;02:00;DIE APOKALYPTISCHEN REITER;genre;DE;1984
+        fields = dict(enumerate(row))
+        day = fields.get(0, 'unknown day')
+        stage = fields.get(1, 'unknown stage')
+        start = fields.get(2, '12:34')
+        starth, startm = [int(x) for x in start.split(':')]
+        end = fields.get(3, '12:35')
+        endh, endm = [int(x) for x in end.split(':')]
+        if starth < DAY_ROLLOVER_HOUR:
+            starth += 24
+        if endh < DAY_ROLLOVER_HOUR:
+            endh += 24
+        duration = (endh - starth) * 60 + (endm - startm)
+        name = fields.get(4, '[no band name]')
+        genre = fields.get(5, '')
+        country = fields.get(6, '')
+        year = fields.get(7, '')
+        if day not in content['days']:
+            content['days'][day] = {
+                'stages': {},
+                'minhour': 24,
+                'maxhour': 0,
             }
-            content['days'][day]['stages'][stage]['bands'].append(band)
+        content['days'][day]['minhour'] = min(content['days'][day]['minhour'], starth)
+        content['days'][day]['maxhour'] = max(content['days'][day]['maxhour'], endh)
+        if stage not in content['days'][day]['stages']:
+            content['days'][day]['stages'][stage] = {
+                'bands': []
+            }
+        band = {
+            'name': name,
+            'starth': starth,
+            'startm': startm,
+            'endh': endh,
+            'endm': endm,
+            'duration': duration,
+            'genre': genre,
+            'country': country,
+            'year': year,
+        }
+        content['days'][day]['stages'][stage]['bands'].append(band)
 
     print(template.render(content))
 
